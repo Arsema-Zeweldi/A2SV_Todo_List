5b56202abc0867a24a537eaf381961a9cd6a785a
Arsema-Zeweldi <arsugeb@gmail.com>
2025-10-28 22:21:12 +0300

    Add-Edit-functionality

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # A2SV Todo List
 
-A simple, client-side Todo List web app that demonstrates basic add and remove task functionality.
+A simple Todo List web app that demonstrates basic add, edit and remove task functionalites.
+
+![alt text](image-5.png)
 
 ## What it does
 
-- Add a task
-- Display tasks
-- Remove a task
+- Add a task ![alt text](image-1.png)
+- Display tasks ![alt text](image-2.png)
+- Edit tasks ![alt text](image-3.png)
+- Remove a task ![alt text](image-4.png)
 
 ## How to run
 
 1. Open "index.html" in a browser.
 2. Enter task name on the input field and click the "Add Task" button.
-3. Click any task in the list to remove it.
+3. Click the "Edit" anchor tag below each list item to edit the task.
+4. Click any task in the list to remove it.
--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const myButton = document.querySelector("button");
 const myInput = document.querySelector("input");
 const myList = document.querySelector("ul");
-const myItems = document.querySelector("ul");
 
 myButton.addEventListener("click", function () {
-  myList.innerHTML += `<li id=${myInput.value}>${myInput.value}</li>`;
+  myList.innerHTML += `<li id=${myInput.value}>${myInput.value}</li> <a href="#" class=${myInput.value}>Edit</a>`;
   myInput.value = "";
 });
 
-myItems.addEventListener("click", function (e) {
-  document.querySelector(`#${e.target.id}`).remove();
+myList.addEventListener("click", function (e) {
+  if (e.target.tagName == "A") {
+    const newTask = prompt("Edit your task:", e.target.className);
+    if (newTask != null && newTask != "") {
+      document.getElementById(`${e.target.className}`).innerText = newTask;
+      document.getElementById(`${e.target.className}`).id = newTask;
+      document.getElementsByClassName(`${e.target.className}`)[0].className =
+        newTask;
+    }
+  } else {
+    const confirmDelete = confirm("Are you sure you want to delete this task?");
+    if (confirmDelete) {
+      document.getElementById(`${e.target.id}`).remove();
+      document.getElementsByClassName(`${e.target.id}`)[0].remove();
+    }
+  }
 });
